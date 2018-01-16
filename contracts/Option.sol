38b9fb7a44f5da118a60dfd10c649ee0b841318e
satyam <satyam@secureblocks.io>
2018-01-16 14:07:44 +0530

    desctiption of function added

--- contracts/Option.sol
+++ contracts/Option.sol
@@ -40,7 +40,10 @@ contract Option is IOption, IERC20 {
 
     mapping(address => traderData) public Traders;
 
-// Constructor
+    /**
+     * @dev `Option` Constructor
+     */
+
     function Option( 
         address _baseToken, 
         address _quoteToken, 
@@ -61,6 +64,14 @@ contract Option is IOption, IERC20 {
         QT = IERC20(quoteToken);
     }
 
+    /**
+     * @dev `issueOption` Use to issue option or generate the option called only by the owner of the option
+     * @param _optionsOffered No. of options need to be generated
+     * @param _premium Amount to be paid by the trader to buy the option
+     * @param _expiry Timestamp when option get expired
+     */
+
+
     function issueOption(uint256 _optionsOffered, uint256 _premium, uint256 _expiry) public {
         require(isOptionIssued == false);
         require(msg.sender == writer);
@@ -77,6 +88,11 @@ contract Option is IOption, IERC20 {
         LogOptionsIssued(optionsOffered, expiry, premium);
     }
 
+    /**
+     * @dev `incOffering` Use to generate the more option supply in between the time boundation of the option
+     * @param _extraOffering No. of options need to generate
+     */
+
     function incOffering(uint256 _extraOffering) public {
         require(msg.sender == writer);
         require(expiry > now);
@@ -86,6 +102,13 @@ contract Option is IOption, IERC20 {
         balances[this] = balances[this].add(_extraOffering);
         LogOptionsIssued(optionsOffered, expiry, premium);
     }
+
+    /**
+     * @dev `tradeOption` This function use to buy the option
+     * @param _trader Address of the buyer who buy the option
+     * @param _amount No. of option trader buy
+     * @return bool
+     */
 
     function tradeOption(address _trader, uint256 _amount) public returns(bool) {
         require(_amount > 0);
@@ -97,6 +120,14 @@ contract Option is IOption, IERC20 {
         LogOptionsTrade(_trader, _amount, now);
     }
     
+    /**
+     * @dev `exerciseOption` This function use to excercise the option means to sell the option to the owner again
+     * @param _trader Address of the holder of the option
+     * @param _amount no. of option trader want to exercise
+     * @param _currentPrice price of the baseToken current in the market
+     * @return bool
+     */
+
     function exerciseOption(address _trader, uint256 _amount, uint256 _currentPrice) external returns (bool) {
         require(_amount > 0);
         require(_trader != address(0));
@@ -115,10 +146,16 @@ contract Option is IOption, IERC20 {
         LogOptionsExcercised(_trader, _currentPrice, _amount, now);
     }
 
-     /* @dev send `_value` token to `_to` from `msg.sender`
-    @param _to The address of the recipient
-    @param _value The amount of token to be transferred
-    @return Whether the transfer was successful or not */
+    //////////////////////////////////
+    ////// ERC20 functions
+    /////////////////////////////////
+
+     /** 
+      * @dev send `_value` token to `_to` from `msg.sender`
+      * @param _to The address of the recipient
+      * @param _value The amount of token to be transferred
+      * @return Whether the transfer was successful or not 
+      */
     function transfer(address _to, uint256 _value) public returns (bool) {
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
@@ -126,11 +163,13 @@ contract Option is IOption, IERC20 {
         return true;
     }
 
-    /* @dev send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-      @param _from The address of the sender
-      @param _to The address of the recipient
-      @param _value The amount of token to be transferred
-      @return Whether the transfer was successful or not */
+    /** 
+     * @dev send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value The amount of token to be transferred
+     * @return Whether the transfer was successful or not 
+     */
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
       require(_to != address(0));
       require(_value <= balances[_from]);
@@ -143,25 +182,33 @@ contract Option is IOption, IERC20 {
       return true;
     }
 
-     /* @param _owner The address from which the balance will be retrieved
-    @return The balance */
+    /**
+     * @dev `balanceOf` function used to read the balance of the token holder
+     * @param _owner The address from which the balance will be retrieved
+     * @return The balance 
+     */
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
-    /* @dev `msg.sender` approves `_spender` to spend `_value` tokens
-    @param _spender The address of the account able to transfer the tokens
-    @param _value The amount of tokens to be approved for transfer
-    @return Whether the approval was successful or not */
+    /** 
+     * @dev `msg.sender` approves `_spender` to spend `_value` tokens
+     * @param _spender The address of the account able to transfer the tokens
+     * @param _value The amount of tokens to be approved for transfer
+     * @return Whether the approval was successful or not 
+     */
     function approve(address _spender, uint256 _value) public returns (bool) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    /* @param _owner The address of the account owning tokens
-    @param _spender The address of the account able to transfer the tokens
-    @return Amount of remaining tokens allowed to spent */
+    /** 
+     * @dev `allowance`
+     * @param _owner The address of the account owning tokens
+     * @param _spender The address of the account able to transfer the tokens
+     * @return Amount of remaining tokens allowed to spent 
+     */
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
