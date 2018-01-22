084cf93f658f2c6184df96a28ef3691a19ff30fc
satyam <satyam@secureblocks.io>
2018-01-22 23:31:49 +0530

    test env added

---
+++ test/DerivativeFactory.js
@@ -0,0 +1,8 @@
+const DerivativeFactory = artifacts.require('./DerivativeFactory.sol');
+
+contract("DerivativeFactory",async(accounts)=>{
+    const baseToken = ""
+    const quoteToken = ""
+
+
+});
